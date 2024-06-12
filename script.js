33f69b796ce92f33a032b575570b906136ed52e1
GAYmerbrrr <160092359+GAYmerbrrr@users.noreply.github.com>
2024-06-12 16:57:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+
 const showMore = document.getElementById("showMore");
 const btn = document.getElementById("btn");
 const sideMenu = document.getElementById("sideMenu");
 const closeBtn = document.getElementById("closeBtn");
+const daftarMenu = document.querySelector(".dftr-mnu")
 
 sideMenu.style.width = "0";
 sideMenu.style.paddingLeft = "0";
 
 
 function openMenus(){
-    sideMenu.style.width = "100%";
+    sideMenu.style.width = "60%";
     sideMenu.style.paddingLeft = "20px";
+    daftarMenu.style.display = "block"
 }
 
 closeBtn.addEventListener("click", function(){
     sideMenu.style.width = "0";
     sideMenu.style.paddingLeft = "0";
+    daftarMenu.style.display = "none"
 });
 
 function show() {
